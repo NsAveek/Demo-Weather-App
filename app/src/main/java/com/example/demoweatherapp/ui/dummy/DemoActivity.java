ff7c680d46681af25f32a5dc5aead3cd7efd09ed
Aveek <nsaveek@gmail.com>
2021-01-22 13:41:20 +0800

    Updated the codebase

--- app/src/main/java/com/example/demoweatherapp/ui/dummy/DemoActivity.java
+++ app/src/main/java/com/example/demoweatherapp/ui/dummy/DemoActivity.java
@@ -1,4 +1,18 @@
 package com.example.demoweatherapp.ui.dummy;
 
-public class DemoActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.demoweatherapp.R;
+
+public class DemoActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_show_case);
+//        setSupportActionBar(findViewById(R.id.toolbar));
+    }
 }
